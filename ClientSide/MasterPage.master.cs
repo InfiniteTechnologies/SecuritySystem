e6bc42f98a4d2013240fab9a03c381ba23927db2
InfiniteTechnologies <info@infinitetechnologies.in>
2014-02-21 09:22:15 +0530

    New Version Client Side.

--- ClientSide/MasterPage.master.cs
+++ ClientSide/MasterPage.master.cs
@@ -6,15 +6,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
 using System.Xml.Linq;
 
-public partial class dashboard : System.Web.UI.Page
+public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
 }
